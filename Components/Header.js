2f86f1ed07916719330b7f4de8fbe9485809b490
enryu-maker <akifkhan60067@gmail.com>
2022-06-06 00:09:43 +0530

    chnages

--- Components/Header.js
+++ Components/Header.js
@@ -5,7 +5,6 @@ import {
     Image,
     Platform
 } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { COLORS, FONTS } from "./Constants"
 
 const Header = ({ containerStyle, title, titleStyle, leftComponent, rightComponent,img ,imgstyle}) => {
@@ -27,7 +26,7 @@ const Header = ({ containerStyle, title, titleStyle, leftComponent, rightCompone
             }
             {title?
             (<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center',}}>
-            <Text style={{  ...FONTS.h2, ...titleStyle}}>{title}</Text>
+            <Text style={{  ...FONTS.h2, ...titleStyle,color:COLORS.black}}>{title}</Text>
         </View>):(
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center',}}>
                 <Image
